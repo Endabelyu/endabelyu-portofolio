03aee9a68a56b21ea41c64b69d00ea2edf8c4065
endabelyu <endabelyuproject@gmail.com>
2023-11-15 22:33:06 +0700

    feat: adjust styling in lyout and footer mobile version

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-slate-50 p-2  sm:p-10">
-        <div className="mx-auto lg:w-3/5 px-2  sm:px-8   gap-16  pt-6 rounded-xl sm:gap-20  shadow-card relative">
+        <div className="mx-auto lg:w-3/5 px-2  sm:px-8   gap-16  sm:pt-6 rounded-xl sm:gap-20  shadow-card relative">
           <Navbar />
           {children}
           <Footer />
